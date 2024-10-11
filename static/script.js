a51e07cb8870f62e940f73554cbe15b811dbb5ff
SathishPasapugazh <64374131+SathishPasapugazh@users.noreply.github.com>
2024-10-11 17:42:36 -0400

    Update script.js

--- static/script.js
+++ static/script.js
@@ -58,7 +58,7 @@ function formatToNC() {
     reader.readAsText(file);
 }
 
-function skill_matrix() {
+function skillMatrix() {
     const fileInput = document.getElementById('resumeFile');
     if (fileInput.files.length === 0) {
         alert("Please upload a resume first.");
