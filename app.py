9f5fa53aae6a9d197b79421176cc6573b0423e81
SathishPasapugazh <64374131+SathishPasapugazh@users.noreply.github.com>
2025-04-30 17:07:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,51 +1,19 @@
-from PyQt5.QtWidgets import (QApplication, QMainWindow, QToolBar, 
-                            QLineEdit, QAction)
-from PyQt5.QtWebEngineWidgets import QWebEngineView
-from PyQt5.QtCore import QUrl
-import sys
+from flask import Flask, render_template, request
+import webbrowser
 
-class MiniBrowser(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle("Mini Browser")
-        self.setGeometry(100, 100, 800, 600)
-        
-        self.browser = QWebEngineView()
-        self.browser.setUrl(QUrl("https://secure.covendis.com/cov/covendis.htm"))
-        self.setCentralWidget(self.browser)
-        
-        # Navigation bar
-        nav_bar = QToolBar()
-        self.addToolBar(nav_bar)
-        
-        back_btn = QAction("Back", self)
-        back_btn.triggered.connect(self.browser.back)
-        nav_bar.addAction(back_btn)
-        
-        forward_btn = QAction("Forward", self)
-        forward_btn.triggered.connect(self.browser.forward)
-        nav_bar.addAction(forward_btn)
-        
-        reload_btn = QAction("Reload", self)
-        reload_btn.triggered.connect(self.browser.reload)
-        nav_bar.addAction(reload_btn)
-        
-        self.url_bar = QLineEdit()
-        self.url_bar.returnPressed.connect(self.navigate_to_url)
-        nav_bar.addWidget(self.url_bar)
-        
-        self.browser.urlChanged.connect(self.update_url)
-    
-    def navigate_to_url(self):
-        url = self.url_bar.text()
-        if not url.startswith(('http://', 'https://')):
-            url = 'https://' + url
-        self.browser.setUrl(QUrl(url))
-    
-    def update_url(self, q):
-        self.url_bar.setText(q.toString())
+app = Flask(__name__)
 
-app = QApplication(sys.argv)
-window = MiniBrowser()
-window.show()
-sys.exit(app.exec_())
+@app.route("/")
+def home():
+    return render_template("browser.html")
+
+@app.route("/navigate", methods=["POST"])
+def navigate():
+    url = request.form.get("url")
+    if not url.startswith(("http://", "https://")):
+        url = f"https://{url}"
+    webbrowser.open(url)  # Opens in user's default browser
+    return {"status": "success", "url": url}
+
+if __name__ == "__main__":
+    app.run(debug=True)
